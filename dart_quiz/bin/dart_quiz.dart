eee2d1205d048be34b23a4409c4fb916b6b3177e
Felipe Nogueira Casimiro <lipecasimiro@gmail.com>
2024-06-03 09:46:35 -0300

    Delete dart_quiz directory

--- dart_quiz/bin/dart_quiz.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_quiz/dart_quiz.dart' as dart_quiz;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_quiz.calculate()}!');
-}
--- dart_quiz/test/dart_quiz_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_quiz/dart_quiz.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
